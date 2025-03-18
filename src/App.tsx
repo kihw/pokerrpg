a46cea7beb61b777c8ce0858a7d593bf44a5eb25
VaL <kihwouih@gmail.com>
2025-03-18 18:34:53 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import PokerRPG from "./components/PokerRPG";
 import "./styles/global.css";
 import { GameProvider } from "./context/GameContext";
+import { ProgressionProvider } from "./context/ProgressionContext";
 
 const App: React.FC = () => {
   return (
-    <GameProvider>
-      <div className="min-h-screen bg-gradient-to-b from-green-900 to-green-800 flex items-center justify-center">
-        <PokerRPG />
-      </div>
-    </GameProvider>
+    <ProgressionProvider>
+      <GameProvider>
+        <div className="min-h-screen bg-gradient-to-b from-green-900 to-green-800 flex items-center justify-center">
+          <PokerRPG />
+        </div>
+      </GameProvider>
+    </ProgressionProvider>
   );
 };
 
